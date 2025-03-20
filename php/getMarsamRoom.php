a03e1ebdb4b7993d020fe6ea2538d7b8b342b8fd
Dave <90359076+DaveKX@users.noreply.github.com>
2025-03-20 17:08:06 +0000

    php + flutter do stor

---
+++ php/getMarsamRoom.php
@@ -0,0 +1,28 @@
+<?php
+$db = "pisid"; 
+$dbhost = "localhost"; 
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$conn = mysqli_connect($dbhost, $username, $password, $db);// Create connection
+
+if (!$conn) {// Check connection
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM `ocupaçãolabirinto` WHERE `IDJogo` = (SELECT MAX(`IDJogo`) FROM `ocupaçãolabirinto`) ORDER BY `Sala`;";
+
+$result = mysqli_query($conn, $sql);// Execute the query
+$response = array();
+if (mysqli_num_rows($result) > 0) {// Check if any row is returned
+     while ($row = mysqli_fetch_assoc($result)) {//Iterates trough the result and puts entries in response
+        array_push($response, $row);
+    }
+} else {
+    $response = array();// If no rows are returned, initialize an empty response
+}
+
+mysqli_close($conn);// Close the connection
+
+echo json_encode($response);// Convert the response array to JSON format
+?>
